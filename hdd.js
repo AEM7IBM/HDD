a8a4bbb13164c9da8407d23558763a6c239e7e7a
Aaron <a.genevision14@gmail.com>
2024-01-16 14:58:50 -0600

    Continued developing header section, added javascript functionality onto company icons

--- hdd.js
+++ hdd.js
@@ -1,1 +1,14 @@
-const companyIcons = document.querySelectorAll('company-icons i');
+const companyIcons = document.querySelectorAll('.company-icons i');
+let i = 1;
+
+setInterval(() => {
+    i++
+    const companyIcon = document.querySelector('.company-icons .change');
+    companyIcon.classList.remove('change');
+    if(i > companyIcons.length) {
+        companyIcons[0].classList.add('change');
+        i = 1;
+    } else {
+        companyIcon.nextElementSibling.classList.add('change');
+    }
+}, 4000);
